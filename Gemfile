2f191eb5a5a1e38395ca1bd13c865c49b66cc439
André Herculano <andresilveirah@gmail.com>
2016-09-25 13:00:21 +0200

    Create website resource (#1)
    * update code styles rules
    
    * add rails-controller-testing gem
    
    * remove jquery from application.js and remove comments
    
    * create Website model
    
    * add jquery-rails gem back
    
    * add basic CRUD for Website

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 gem 'pg', '~> 0.18'
 gem 'puma', '~> 3.0'
+gem 'jquery-rails'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
@@ -13,6 +14,10 @@ group :development, :test do
   gem 'byebug', platform: :mri
   gem 'rspec-rails', '~> 3.5'
   gem 'rubocop', '~> 0.43.0', require: false
+end
+
+group :test do
+  gem 'rails-controller-testing'
 end
 
 group :development do
---
+++ spec/controllers/websites_controller_spec.rb
@@ -0,0 +1,75 @@
+require 'rails_helper'
+
+RSpec.describe WebsitesController, type: :controller do
+  let(:valid_attributes) {
+    { url: 'http://www.codebikeandmore.com' }
+  }
+
+  let(:invalid_attributes) {
+    { url: '' }
+  }
+
+  describe 'GET #index' do
+    it 'assigns all websites as @websites' do
+      website = Website.create! valid_attributes
+      get :index
+      expect(assigns(:websites)).to eq([website])
+    end
+  end
+
+  describe 'GET #show' do
+    it 'assigns the requested website as @website' do
+      website = Website.create! valid_attributes
+      get :show, params: { id: website.to_param }
+      expect(assigns(:website)).to eq(website)
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid params' do
+      it 'creates a new Website' do
+        expect {
+          post :create, params: { website: valid_attributes }
+        }.to change(Website, :count).by(1)
+      end
+
+      it 'assigns a newly created website as @website' do
+        post :create, params: { website: valid_attributes }
+        expect(assigns(:website)).to be_a(Website)
+        expect(assigns(:website)).to be_persisted
+      end
+
+      it 'redirects to the created website' do
+        post :create, params: { website: valid_attributes }
+        expect(response).to redirect_to(Website.last)
+      end
+    end
+
+    context 'with invalid params' do
+      it 'assigns a newly created but unsaved website as @website' do
+        post :create, params: { website: invalid_attributes }
+        expect(assigns(:website)).to be_a_new(Website)
+      end
+
+      it 're-renders the "new" template' do
+        post :create, params: { website: invalid_attributes }
+        expect(response).to render_template('index')
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'destroys the requested website' do
+      website = Website.create! valid_attributes
+      expect {
+        delete :destroy, params: {id: website.to_param }
+      }.to change(Website, :count).by(-1)
+    end
+
+    it 'redirects to the websites list' do
+      website = Website.create! valid_attributes
+      delete :destroy, params: {id: website.to_param }
+      expect(response).to redirect_to(websites_url)
+    end
+  end
+end
