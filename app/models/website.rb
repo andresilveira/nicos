f3a01d6c35b9383a64ea5f07c06d99b51e125478
Andre Herculano <aherculano@babbel.com>
2016-09-26 17:44:38 +0200

    use the url validator on Website model

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,3 +1,12 @@
 class Website < ApplicationRecord
   validates :url, presence: true
+  validate  :url_valid?
+
+  private
+
+  def url_valid?
+    unless Validators::Url.valid?(url)
+      errors.add(:url, 'is not a valid url')
+    end
+  end
 end
